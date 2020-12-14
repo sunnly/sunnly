18b83354df957a611cf6ac4b3850b402b15064fa
王宏旭 <wanghongxu_java@qq.com>
2020-12-14 22:51:12 +0800

    实体封装

--- sunnly-modules/sunnly-modules-demo/sunnly-modules-auth/src/main/java/wang/sunnly/modules/auth/service/impl/AuthServiceImpl.java
+++ sunnly-modules/sunnly-modules-demo/sunnly-modules-auth/src/main/java/wang/sunnly/modules/auth/service/impl/AuthServiceImpl.java
@@ -1,7 +1,5 @@
 package wang.sunnly.modules.auth.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -38,30 +36,25 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public long lockedTime(String username){
         return redisTemplate.opsForValue().increment("locked:" + username, 0);
-//        return redisTemplate.boundValueOps("locked:"+username).getExpire();
     }
 
     @Override
     public void lockedUser(String username){
         redisTemplate.opsForValue().set("locked:"+username, "locked", 5, TimeUnit.MINUTES);
-//        redisTemplate.boundValueOps("locked:"+username).set("locked",  5, TimeUnit.MINUTES);
     }
 
     @Override
     public void setValidateCode(String username, String code){
         redisTemplate.opsForValue().set("validate:code:" + username, code,5, TimeUnit.MINUTES);
-//        redisTemplate.boundValueOps("validate:code:" + username).set(code, 5, TimeUnit.MINUTES);
     }
     @Override
     public String getValidateCode(String username){
         return redisTemplate.opsForValue().get("validate:code:" + username);
-//        return redisTemplate.boundValueOps("validate:code:" + username).get();
     }
 
     @Override
     public void removeValidateCode(String username){
         redisTemplate.opsForValue().set("validate:code:" + username, "", 1, TimeUnit.SECONDS);
-//        redisTemplate.boundValueOps("validate:code:" + username).expire(1, TimeUnit.SECONDS);
     }
 
     @Override
@@ -75,14 +68,12 @@ public class AuthServiceImpl implements AuthService {
         if (validate.getCode() == AuthAssertEnum.USERNAME_PASSWORD_NOT_MATCH.getCode()){
             //用户名或密码错误
             String lockedCount = redisTemplate.opsForValue().get("validate:locked:" + username);
-//            String lockedCount = redisTemplate.boundValueOps("validate:locked:" + username).get();
             int count = 0;
             if (lockedCount != null){
                 count = Integer.parseInt(lockedCount);
             }
             if (count < 5){
                 redisTemplate.opsForValue().set("validate:locked:"+username, (++count)+"", 1, TimeUnit.HOURS);
-//                redisTemplate.boundValueOps("validate:locked:"+username).set((++count)+"", 1, TimeUnit.HOURS);
             }else{
                 lockedUser(username);
             }
